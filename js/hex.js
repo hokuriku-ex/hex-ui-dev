3ece8162f467faafbe1269b403e99d6a0e960a87
hokuriku-ex <design-room@hokuriku-ex.co.jp>
2026-08-07 06:38:06 +0000

    2026年8月7日 15:38

--- js/hex.js
+++ js/hex.js
@@ -2885,7 +2885,7 @@ hexLoad(function(){
           var title=sections[i].querySelector('.hex-anchor-target');
           if(!title)continue;
           if(title.textContent.trim()!==anchorParam)continue;
-          var offset=window.innerWidth<=768?190:190;
+          var offset=window.innerWidth<=768?180:180;
           var top=sections[i].getBoundingClientRect().top+window.pageYOffset-offset;
           window.scrollTo({
             top:top,
