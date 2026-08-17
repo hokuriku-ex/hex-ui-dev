cab0b1f3bcc89a8861207d65697383010930ff53
hokuriku-ex <design-room@hokuriku-ex.co.jp>
2026-08-17 01:56:42 +0000

    2026年8月17日 10:56

--- js/hex.js
+++ js/hex.js
@@ -2381,7 +2381,7 @@ hexReady(function(){
     if(!companyText.querySelector('.footer-company-name')){
       var companyName=document.createElement('div');
       companyName.className='footer-company-name';
-      companyName.textContent='Hokuriku-EX. All Rights Reserved.';
+      companyName.textContent='北陸エクステリア株式会社';
       companyText.prepend(companyName);
     }
 
